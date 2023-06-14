ac2b3c749c7a3013b3f4e1b4f64a7990d4ebb5d4
drdevlin <drdevlin@fastmail.com>
2023-06-14 09:36:31 -0400

    feat: add unauth context to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { UnauthorizedContext } from '@/components/Unauthorized';
 
 import type { AppProps } from 'next/app';
 
@@ -7,9 +9,21 @@ import '@/styles/globals.css';
 export const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [unauthorizedVisibility, setUnauthorizedVisibility] = useState(false);
+  const showUnauthorized = () => setUnauthorizedVisibility(true);
+  const hideUnauthorized = () => setUnauthorizedVisibility(false);
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <UnauthorizedContext.Provider
+        value={{
+          isVisible: unauthorizedVisibility,
+          show: showUnauthorized,
+          hide: hideUnauthorized,
+        }}
+      >
+        <Component {...pageProps} />
+      </UnauthorizedContext.Provider>
     </QueryClientProvider>
   );
 }
