8526e8cbbca45578edcf660be2f84b27a6451a93
drdevlin <drdevlin@fastmail.com>
2023-06-19 13:36:18 -0400

    fix: disable apple web app capable

--- components/OscarPartyHead.tsx
+++ components/OscarPartyHead.tsx
@@ -5,7 +5,6 @@ export const OscarPartyHead = () => {
     <Head>
       <title>Oscar Party</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
   );
