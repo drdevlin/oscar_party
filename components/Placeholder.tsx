fa16042ef59b012f1a6b60d6fada56f608d3b282
drdevlin <drdevlin@fastmail.com>
2024-01-23 13:22:11 -0500

    fix(Placeholder): prevent no keys error

--- components/Placeholder.tsx
+++ components/Placeholder.tsx
@@ -7,7 +7,10 @@ import { Item } from "./Item";
 export const Placeholder = forwardRef<HTMLDivElement>(function Placeholder(props, ref) {
   return (
     <motion.div animate={{ opacity: 1 }} exit={{ opacity: 0 }} ref={ref}>
-      {Array(4).fill(<GhostAnimation><Item><div style={{ height: '2rem' }} /></Item></GhostAnimation>)}
+      <GhostAnimation><Item><div style={{ height: '2rem' }} /></Item></GhostAnimation>
+      <GhostAnimation><Item><div style={{ height: '2rem' }} /></Item></GhostAnimation>
+      <GhostAnimation><Item><div style={{ height: '2rem' }} /></Item></GhostAnimation>
+      <GhostAnimation><Item><div style={{ height: '2rem' }} /></Item></GhostAnimation>
     </motion.div> 
   );
 });
