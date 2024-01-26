abe19f7312235d49c6f4918b3047d3ba05722fbc
drdevlin <drdevlin@fastmail.com>
2024-01-25 21:20:28 -0500

    feat: update head title, viewport, theme-color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Oscar Party</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="rgb(24 24 27)" />
+      </Head>
       <body>
         <Main />
         <NextScript />
